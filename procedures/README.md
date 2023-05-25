b2b079a65b754fd2f96258f6a4a7b5e0812128d7
Patrick Nyatindo <nyatindopatrick@gmail.com>
2023-05-25 08:43:44 +0300

    add procedures utility

---
+++ procedures/README.md
@@ -0,0 +1,32 @@
+# Procedures Utility
+
+This repository contains the utility code for adding value sets to the snowstorm server.
+
+# Run
+
+1. This utility requires Node, npm.
+
+### One time installation:
+
+These steps need to performed ONLY the FIRST TIME you set up this code.
+
+1. Install node/npm (node version: 10.11.0). Preferably use nvm, so that you have control over which project uses which version of node. See:
+
+   * [how to install Node using nvm](https://github.com/nvm-sh/nvm).
+   * [how to install NodeJS on mac](https://www.newline.co/@Adele/how-to-install-nodejs-and-npm-on-macos--22782681)
+
+### Environment Variables
+
+| Variable           | Description                     | Example                                                                       |
+| ------------------ | ------------------------------- | ----------------------------------------------------------------------------- |
+| SAVE_VALUESET_URL  | Snowstorm server value set url | https://snowstorm.snomed.mybahmni.in/fhir/ValueSet                            |
+| BAHMNI_FHIR_SERVER | Bahmni procedures endpoint      | https://{bahmni.instance.com}/openmrs/ws/rest/v1/terminologyServices/valueSet |
+| USERNAME           | Bahmni username                 | superman                                                                      |
+| USERNAME           | Bahmi password                  | Admin123                                                                      |
+
+### Commands
+
+Run these commands from within the root folder.
+
+1. `npm install`
+2. `npm start`
---
+++ procedures/src/services/formatter.js
@@ -0,0 +1,58 @@
+import csv from 'csv-parser';
+
+function formatCategory(category) {
+  return `bahmni-procedures-${category
+    .toLowerCase()
+    .replace(/ /g, '-').replace(/&/g, 'and')}`;
+}
+
+export const convertToJSON = (csvFile) => {
+  const results = [];
+  return new Promise((resolve, reject) => {
+    csvFile
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => resolve(results))
+      .on('error', (err) => reject(err));
+  });
+};
+
+export function convertToValueSet(data) {
+  const bodySiteCategories = [
+    ...new Set(data.map((item) => item['Body Site Categories'])),
+  ];
+
+  const formattedData = bodySiteCategories.map((category) => {
+    const valueSet = {
+      resourceType: 'ValueSet',
+      id: formatCategory(category),
+      url: `http://bahmni.org/fhir/ValueSet/${formatCategory(
+        category,
+      )}`,
+      version: '0.1',
+      name: formatCategory(category),
+      title: category,
+      description: `List of possible procedures on the ${formatCategory(
+        category,
+      )}`,
+      status: 'draft',
+      experimental: true,
+      compose: {
+        include: [
+          {
+            system: 'http://snomed.info/sct',
+            concept: data
+              .filter((item) => item['Body Site Categories'] === category)
+              .map((item) => ({
+                code: item['Target code'],
+              }))
+              .filter((item) => item.code),
+          },
+        ],
+      },
+    };
+    return valueSet;
+  }).filter((item) => item.compose.include[0]?.concept.length > 0);
+
+  return formattedData;
+}
